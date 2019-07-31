19a6426e34a77ad91741e3e811cb658d9e66210e
ev081geniy <ev081geniy@gmail.com>
2019-08-01 01:07:07 +0300

    Восьмое задание

--- js/picture.js
+++ js/picture.js
@@ -1,11 +1,19 @@
 'use strict';
 
 (function () {
-  var SUM_PHOTOS = 25;
+  var postArray = [];
+
+  var filters = {
+    'filter-popular': 'filterPopular',
+    'filter-new': 'filterNew',
+    'filter-discussed': 'filterDiscussed'
+  };
 
   var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var fragment = document.createDocumentFragment();
   var picturesList = document.querySelector('.pictures');
+  var imgFilters = document.querySelector('.img-filters');
+  var imgFiltersForm = document.querySelector('.img-filters__form');
 
   var renderPhoto = function (post) {
     var clonePicture = pictureTemplate.cloneNode(true);
@@ -15,13 +23,50 @@
     fragment.appendChild(clonePicture);
   };
 
-  var renderPhotos = function (posts) {
-    for (var i = 0; i < SUM_PHOTOS; i++) {
-      var post = posts[i];
-      renderPhoto(post);
-    }
+  var renderPictures = function (pictures) {
+    pictures.forEach(function (picture) {
+      renderPhoto(picture);
+    });
     picturesList.appendChild(fragment);
   };
 
-  window.load(renderPhotos);
+  var removePictures = function () {
+    var pictures = picturesList.querySelectorAll('.picture');
+
+    pictures.forEach(function (picture) {
+      picturesList.removeChild(picture);
+    });
+  };
+
+  var refreshPictures = function (array) {
+    removePictures();
+    renderPictures(array);
+  };
+
+  var changeClassActive = function (elem) {
+    imgFilters.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+    elem.classList.add('img-filters__button--active');
+  };
+
+  var onClickFiltersButton = function (evt) {
+    if (evt.target.tagName === 'BUTTON') {
+      var filterName = filters[evt.target.id];
+      var newArray = window.filters[filterName](postArray);
+
+      changeClassActive(evt.target);
+      window.debounce(function () {
+        refreshPictures(newArray);
+      });
+    }
+  };
+
+  var onSuccessPhotos = function (array) {
+    postArray = array;
+    imgFilters.classList.remove('img-filters--inactive');
+    renderPictures(postArray);
+
+    imgFiltersForm.addEventListener('click', onClickFiltersButton);
+  };
+
+  window.load(onSuccessPhotos);
 })();

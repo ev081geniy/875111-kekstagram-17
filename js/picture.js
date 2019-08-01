5f73fc8b5b6ef608644127f15bf2d69b4bf7cb6b
ev081geniy <ev081geniy@gmail.com>
2019-08-01 19:33:12 +0300

    Задание 8.1

--- js/picture.js
+++ js/picture.js
@@ -65,8 +65,53 @@
     imgFilters.classList.remove('img-filters--inactive');
     renderPictures(postArray);
 
+    renderBigPic(postArray[0]);
+    bigPicture.classList.remove('hidden');
+
     imgFiltersForm.addEventListener('click', onClickFiltersButton);
   };
 
   window.load(onSuccessPhotos);
+
+  var bigPicture = document.querySelector('.big-picture');
+  var socialCommentCount = bigPicture.querySelector('.social__comment-count');
+  var commentsLoader = bigPicture.querySelector('.comments-loader');
+  var socialComments = bigPicture.querySelector('.social__comments');
+  var socialComment = bigPicture.querySelector('.social__comment');
+
+  var createComment = function (array) {
+    var cloneComment = socialComment.cloneNode(true);
+    cloneComment.querySelector('.social__picture').src = array.avatar;
+    cloneComment.querySelector('.social__text').textContent = array.message;
+
+    fragment.appendChild(cloneComment);
+  };
+
+  var renderBigPic = function (post) {
+    bigPicture.querySelector('IMG').src = post.url;
+    bigPicture.querySelector('.likes-count').textContent = post.likes;
+    bigPicture.querySelector('.comments-count').textContent = post.comments.length;
+    bigPicture.querySelector('.social__caption').textContent = post.description;
+
+    createComments(post.comments);
+
+    socialCommentCount.classList.add('visually-hidden');
+    commentsLoader.classList.add('visually-hidden');
+  };
+
+  var createComments = function (comments) {
+    comments.forEach(function (comment) {
+      createComment(comment);
+    });
+    removeComments();
+    socialComments.appendChild(fragment);
+  };
+
+  var removeComments = function () {
+    var comments = socialComments.querySelectorAll('.social__comment');
+
+    comments.forEach(function (comment) {
+      socialComments.removeChild(comment);
+    });
+  };
 })();
